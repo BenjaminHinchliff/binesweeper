f07dfc236ca865bddd6981acdb5a592bfff0048a
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-02-10 23:43:59 -0800

    add grid

---
+++ src/lib/components/Grid.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let width: number;
+	export let height: number;
+</script>
+
+<div class="grid" style:grid-template-columns="repeat({width}, 1fr [col-start])">
+	{#each Array(width) as _, i}
+		{#each Array(height) as _, j}
+			<button class="grid-cell" on:click={console.log} on:contextmenu|preventDefault={console.log}
+				>{i}, {j}</button
+			>
+		{/each}
+	{/each}
+</div>
+
+<style lang="scss">
+	.grid {
+		display: grid;
+	}
+
+	.grid-cell {
+		font-size: 4vw;
+		aspect-ratio: 1;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Grid from '$lib/components/Grid.svelte';
+</script>
+
+<Grid width={10} height={5} />
+
